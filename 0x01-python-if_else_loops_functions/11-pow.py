bb47b417c854ae8f0fcf9a84164a525f1a077107
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-11-28 15:38:36 -0800

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 def pow(a, b):
-    res = 1
-    for i in range(b):
-        res *= a
-    return res
+    # Base case: any number to the power of 0 is 1
+    if b == 0:
+        return 1
+
+    # Initialize result to the base
+    result = a
+
+    # Multiply the result by base 'b' times
+    for _ in range(1, abs(b)):
+        result *= a
+
+    # If the exponent is negative, take the reciprocal
+    if b < 0:
+        result = 1 / result
+
+    return result
