be7d8a64966b64d1bbb5c3a6d1f1d698bdd02110
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-12-04 05:57:15 -0800

    12-switch.py

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,7 +5,6 @@
  * @head: pointer to the head of the list
  * Return: 0 if not a palindrome, 1 if a palindrome
  */
-
 int is_palindrome(listint_t **head)
 {
     listint_t *slow = *head;
@@ -48,5 +47,49 @@ int is_palindrome(listint_t **head)
     }
 
     return result;
+}
+
+/**
+ * reverse_list - reverses a linked list
+ * @head: pointer to the head of the list
+ * Return: pointer to the new head of the reversed list
+ */
+listint_t *reverse_list(listint_t **head)
+{
+    listint_t *prev = NULL;
+    listint_t *current = *head;
+    listint_t *next;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+
+    return *head;
+}
+
+/**
+ * compare_lists - compares two linked lists
+ * @list1: pointer to the head of the first list
+ * @list2: pointer to the head of the second list
+ * Return: 1 if lists are equal, 0 if not
+ */
+int compare_lists(listint_t *list1, listint_t *list2)
+{
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->n != list2->n)
+            return 0;
+
+        list1 = list1->next;
+        list2 = list2->next;
+    }
+
+    return (list1 == NULL && list2 == NULL);
 }
 
