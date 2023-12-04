30f026a0ada3ec8071b55bfef9c546c1a2ed16c1
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-12-04 05:34:00 -0800

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     # Return a new list with True or False,
-    #depending on whether the integer at the
-    #same position in the original list is a multiple of 2
+    # depending on whether the integer at the
+    # same position in the original list is a multiple of 2
     return [num % 2 == 0 for num in my_list]
