4747ba02601263e9b5d492a0c6f6b36b5e66b317
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-12-04 05:39:30 -0800

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    # If idx is negative or out of range, nothing changes (returns the same list)
+    # If idx is negative or out of range, nothing change
     if idx < 0 or idx >= len(my_list):
         return my_list
 
