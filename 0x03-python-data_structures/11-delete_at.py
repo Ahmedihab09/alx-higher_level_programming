7a2f467d17f7f2db1357555084827aa28fcc2cc5
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-12-04 05:35:04 -0800

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    # If idx is negative or out of range, nothing changes (returns the same list)
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    # Create a new list excluding the element at the specified index
+    new_list = my_list[:idx] + my_list[idx+1:]
+    return new_list
