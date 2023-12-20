fe6a0bdd97fe617b3b8568cd083891f5256c721d
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-12-19 17:39:18 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''Sqaure Class'''
+
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): Private instance attribute representing the size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
