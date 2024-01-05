01d1e746f51891176e086975e9c55976951abc81
Ahmedihab09 <ahmed.ihab353@gmail.com>
2024-01-04 16:00:58 -0800

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@ class Rectangle:
     Class that defines a rectangle.
     """
 
-    number_of_instances = 0  # Public class attribute
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """
