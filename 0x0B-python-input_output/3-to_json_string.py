68b2320c9134a7a4dff052a6bcce3f3b590d0ea0
Ahmedihab09 <ahmed.ihab353@gmail.com>
2024-01-09 08:23:50 -0800

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,25 +4,6 @@
 
 
 def to_json_string(my_obj):
-    '''
-    Returns a basic JSON representation of a simple Python dictionary object.
-    This function does not handle complex or nested objects and is not a
-    substitute for the json.dumps method.
-    '''
-    if isinstance(my_obj, dict):
-        items = []
-        for key, value in my_obj.items():
-            json_key = str(key)
-            if isinstance(value, str):
-                json_value = '"' + value + '"'
-            elif value is None:
-                json_value = "null"
-            elif isinstance(value, bool):
-                json_value = "true" if value else "false"
-            else:
-                json_value = str(value)
-            items.append(f'"{json_key}": {json_value}')
-        json_string = "{" + ", ".join(items) + "}"
-        return json_string
-    else:
-        raise TypeError("Object must be a dictionary")
+    '''Returns the JSON representation of an object (string)'''
+    import json
+    return json.dumps(my_obj)
