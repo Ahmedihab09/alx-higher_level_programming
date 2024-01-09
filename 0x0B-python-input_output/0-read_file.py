e68bd26dd93d57c858b3240e3944e65895736b56
Ahmedihab09 <ahmed.ihab353@gmail.com>
2024-01-09 08:14:07 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ def read_file(filename=""):
     # Use 'with' statement for safe opening and closing of the file
     with open(filename, "r", encoding="utf-8") as file:
         # Read the file line by line and print to stdout
-        for line in file:
-            print(line, end="")
+        print(file.read(), end="")
