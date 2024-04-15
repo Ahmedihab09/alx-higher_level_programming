eb4c33536bddeac49e90ef5416fbf7cb309cf281
Ahmedihab09 <ahmed.ihab353@gmail.com>
2024-04-14 17:32:09 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,46 +1,21 @@
 #!/usr/bin/python3
 
-""" Filter cities by state name """
-
+""" Select all states from database """
 import MySQLdb
-import sys
-
-
-def filter_cities(username, password, db_name, state_name):
-    """ Connects to the database and lists cities of the given state """
-    try:
-        conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            charset="utf8",
-            user=username,
-            passwd=password,
-            db=db_name
-        )
-        cur = conn.cursor()
-        query = "SELECT GROUP_CONCAT(name SEPARATOR ', ') FROM cities \
-                 JOIN states ON cities.state_id = states.id \
-                 WHERE states.name = %s \
-                 ORDER BY cities.id ASC"
-        cur.execute(query, (state_name,))
-        query_row = cur.fetchone()
-        cities = query_row[0] if query_row else None
-        if cities:
-            print(cities)
-        else:
-            print("No cities found for the given state.")
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-    finally:
-        cur.close()
-        conn.close()
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: ./5-filter_cities.py <username>\
-                <password> <db_name> <state_name>")
-        sys.exit(1)
-
-    username, password, db_name, state_name = sys.argv[1:]
-    filter_cities(username, password, db_name, state_name)
+    """ Main function """
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    query = "SELECT cities.name FROM cities \
+            JOIN states ON cities.state_id = states.id\
+            WHERE states.name = %s ORDER BY cities.id ASC"
+    cur.execute(query, (argv[4],))
+    query_rows = cur.fetchall()
+    states = [row[0] for row in query_rows]
+    print(", ".join(states))
+    cur.close()
+    conn.close()
