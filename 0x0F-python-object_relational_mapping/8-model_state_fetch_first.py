4c5c6fe50972f1e6198f8606376fcfe9127b3559
Ahmedihab09 <ahmed.ihab353@gmail.com>
2024-04-14 18:23:52 -0700

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     username, password, db_name = sys.argv[1:]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name), pool_pre_ping=True)
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
@@ -23,4 +24,3 @@ if __name__ == "__main__":
         print("Nothing")
 
     session.close()
-
