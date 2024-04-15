e21fdf8bbcc3c136f3b8bfebabb7d118bbaa74be
Ahmedihab09 <ahmed.ihab353@gmail.com>
2024-04-14 17:55:12 -0700

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,7 +5,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    username, password, db_name = sys.argv[1:]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, db_name))
