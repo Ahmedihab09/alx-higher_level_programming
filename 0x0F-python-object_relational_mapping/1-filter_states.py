7a47d0eb6f74207ffe4455fb2ae7f9fc7f1f7299
Ahmedihab09 <ahmed.ihab353@gmail.com>
2024-04-14 17:01:36 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+""" Select states starting with 'N' from database """
+import MySQLdb
+import sys
+
+
+def filter_states(username, password, db_name):
+    """ Connects to the database and selects states starting with 'N' """
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            charset="utf8",
+            user=username,
+            passwd=password,
+            db=db_name
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+    finally:
+        cur.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./1-filter_states.py <username> <password> <db_name>")
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    filter_states(username, password, db_name)
+
